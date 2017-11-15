7be6bf6608a4307f8159f494b48fd7e36f0a0b01
Seth Bresnett <sgb@softwire.com>
2017-11-15 14:55:05 +0000

    Initial state

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gilded Rose
+
+Gilded Rose refactoring kata in Java - initial Java code from https://github.com/emilybache/GildedRose-Refactoring-Kata
+
---
+++ src/test/java/training/gildedrose/GildedRoseTest.java
@@ -0,0 +1,13 @@
+package training.gildedrose;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GildedRoseTest {
+
+    @Test
+    public void failingTest() {
+        assertThat(1).isEqualTo(2);
+    }
+}
