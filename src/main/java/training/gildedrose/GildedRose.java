0c85bdc34bd52219746ab3630ad3d3ad4be1100c
Seth Bresnett <sgb@softwire.com>
2017-11-16 09:44:09 +0000

    More refactoring

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -1,6 +1,23 @@
 package training.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+    private static final Map<String, QualityRule> QUALITY_RULES = new HashMap<>();
+
+    static {
+        QUALITY_RULES.put(AGED_BRIE, item -> item.quality + (isExpired(item) ? 2 : 1));
+        QUALITY_RULES.put(BACKSTAGE_PASS, GildedRose::newBackstagePassQuality);
+        QUALITY_RULES.put(SULFURAS, item -> item.quality);
+    }
+
+    private static final QualityRule DEFAULT_QUALITY_RULE = item -> item.quality - (isExpired(item) ? 2 : 1);
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -10,25 +27,22 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (isSulfuras(item)) {
-                continue;
+            if (!isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            item.sellIn = item.sellIn - 1;
-
-            if (isAgedBrie(item)) {
-                item.quality += isExpired(item) ? 2 : 1;
-            } else if (isBackstagePass(item)) {
-                item.quality = newBackstagePassQuality(item);
-            } else {
-                item.quality -= isExpired(item) ? 2 : 1;
-            }
+            QualityRule rule = QUALITY_RULES.getOrDefault(item.name, DEFAULT_QUALITY_RULE);
+            item.quality = rule.newQuality(item);
 
             enforceQualityRange(item);
         }
     }
 
     private void enforceQualityRange(Item item) {
+        if (isSulfuras(item)) {
+            return;
+        }
+
         if (item.quality < 0) {
             item.quality = 0;
         }
@@ -37,7 +51,7 @@ class GildedRose {
         }
     }
 
-    private int newBackstagePassQuality(Item pass) {
+    private static int newBackstagePassQuality(Item pass) {
         if (isExpired(pass)) {
             return 0;
         }
@@ -53,19 +67,15 @@ class GildedRose {
         return pass.quality + 1;
     }
 
-    private boolean isExpired(Item item) {
+    private static boolean isExpired(Item item) {
         return item.sellIn < 0;
     }
 
-    private static boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
     private static boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
+        return item.name.equals(SULFURAS);
     }
 
-    private static boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    private interface QualityRule {
+        int newQuality(Item item);
     }
 }
