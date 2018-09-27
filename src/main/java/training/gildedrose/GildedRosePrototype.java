dbda755af11d3ff5947c58890eef4dcc874824c4
Taiwo <Taiwo.Olateju@softwire.com>
2018-09-27 17:13:59 +0100

    Tests done and Prototype done

--- src/main/java/training/gildedrose/GildedRosePrototype.java
+++ src/main/java/training/gildedrose/GildedRosePrototype.java
@@ -1,20 +1,14 @@
 package training.gildedrose;
 
-import java.util.ArrayList;
 
 public class GildedRosePrototype {
     private Item[] items;
-    private ArrayList<String> specialtems = new ArrayList<>();
     private int MAX_QUALITY = 50; //sulfuras is the exception stays at 80
     private int MIN_QUALITY = 0;
     private int MAX_SULFURAS_QUALITY = 80;
 
     public GildedRosePrototype(Item[] items) {
         this.items = items;
-        this.specialtems.add("Aged Brie");
-        this.specialtems.add("Backstage passes to a TAFKAL80ETC concert");
-        this.specialtems.add("Sulfuras, Hand of Ragnaros");
-        this.specialtems.add("Conjured");
     }
 
     private void updateAgedBrieQuality(Item item){
@@ -29,17 +23,17 @@ public class GildedRosePrototype {
 
     private void updateBackstagePassQuality(Item item){
         int multiplier = 1;
-        if(item.sellIn <= 10){
+        if(item.sellIn < 10){
             multiplier = 2;
         }
-        if(item.sellIn <= 5){
+        if(item.sellIn < 5){
             multiplier = 3;
         }
         if(item.quality < MAX_QUALITY){
             item.quality += multiplier;
         }
         if(item.sellIn < 0){
-            item.quality = 0;
+            item.quality = MIN_QUALITY;
         }
     }
 
@@ -76,23 +70,25 @@ public class GildedRosePrototype {
     public void updateQuality(){
         for(Item item: this.items){
             updateSellin(item);
-            if(this.specialtems.contains(item.name)){
-                if(item.name.equals("Aged Brie")){
-                    updateAgedBrieQuality(item);
-                }
-                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                    updateBackstagePassQuality(item);
-                }
-                if(item.name.equals("Conjured")){
-                    updateConjuredItemQuality(item);
-                }
-                if(item.name.equals("Sulfuras, Hand of Ragnaros")){
-                    updateSulfurasQuality(item);
-                }
+
+            if(item.name.equals("Aged Brie")){
+                updateAgedBrieQuality(item);
             }
+            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                updateBackstagePassQuality(item);
+            }
+            else if(item.name.contains("Conjured")){
+                updateConjuredItemQuality(item);
+            }
+            else if(item.name.equals("Sulfuras, Hand of Ragnaros")){
+                updateSulfurasQuality(item);
+            }
+
             else{
                 updateNormalItemQuality(item);
             }
+
+            System.out.println(item.toString());
         }
     }
 
--- src/test/java/training/gildedrose/GildedRoseTest.java
+++ src/test/java/training/gildedrose/GildedRoseTest.java
@@ -6,8 +6,158 @@ import static org.assertj.core.api.Assertions.*;
 
 public class GildedRoseTest {
 
+
     @Test
-    public void failingTest() {
-        assertThat(1).isEqualTo(2);
+    public void testThatAgedBrieGoesUpByOne(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Aged Brie", 5, 30);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(31);
     }
+
+    @Test
+    public void testThatAgedBrieGoesUpByTwo(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Aged Brie", -1, 30);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(32);
+    }
+
+    @Test
+    public void testThatAgedBrieHasQualityLimit(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Aged Brie", -1, 50);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(50);
+    }
+
+    @Test
+    public void testThatAgedBrieSellInGoesDown(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Aged Brie", -1, 20);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].sellIn).isEqualTo(-2);
+    }
+
+    @Test
+    public void testThatSulfurasNeverChangesQuality(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Sulfuras, Hand of Ragnaros", 5, 80);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(80);
+    }
+
+    @Test
+    public void testThatSulfurasNeverChangesSellIn(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Sulfuras, Hand of Ragnaros", 50, 80);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].sellIn).isEqualTo(50);
+    }
+
+    @Test
+    public void testThatBackstagePassGoesUpByOne(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 20, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(44);
+    }
+
+    @Test
+    public void testThatBackstagePassGoesUpByTwo(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(45);
+    }
+
+    @Test
+    public void testThatBackstagePassGoesUpByThree(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(46);
+    }
+
+    @Test
+    public void testThatConjuredItemsGoDownByTwo(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Conjured Shoe", 5, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(41);
+
+    }
+
+    @Test
+    public void testThatConjuredItemsGoDownByFour(){
+        Item[] list = new Item[1];
+        list[0] = new Item("Conjured Shoe", -5, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+
+        inventory.updateQuality();
+
+        assertThat(list[0].quality).isEqualTo(39);
+
+    }
+
+    @Test
+    public void goldTest(){
+        Item[] list = new Item[5];
+        list[0] = new Item("Conjured Shoe", -5, 43);
+        list[1] = new Item("Backstage passes to a TAFKAL80ETC concert", -5, 43);
+        list[2] = new Item("Sulfuras, Hand of Ragnaros", -5, 43);
+        list[3] = new Item("Aged Brie", -5, 43);
+        list[4] = new Item("Apple", -5, 43);
+        GildedRosePrototype inventory = new GildedRosePrototype(list);
+
+//        for(i)
+//        inventory.updateQuality();
+
+        //assertThat(list[0].quality).isEqualTo(39);
+
+    }
+
+
+
 }
